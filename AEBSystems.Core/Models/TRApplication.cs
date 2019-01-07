226cc495f139d44d1fb686954c6233541a205263
Mikiee <abulag.michael@gmail.com>
2019-01-07 11:23:43 +0800

    Add project files.

---
+++ AEBSystems.Core/Models/TRApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AEBSystems.Core.Models
+{
+    public class TRApplication :BaseEntity
+    {
+        public string PEL { get; set; }
+        public string InitialApp { get; set; }
+        public string InitialAppDateTime { get; set; }
+        
+    }
+}
